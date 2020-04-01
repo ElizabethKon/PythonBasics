bbc06d14ddcc67932f2e1d48407aa84fe0a88a24
Elizabeth <lizakon@gmail.com>
2020-04-01 14:52:57 +0300

    Задание 2. Решено четыре задачи.

---
+++ Lesson2 ep1.py
@@ -0,0 +1,13 @@
+# Task 1 создать список
+my_list = [1, 'апельсин', 2, 5.55, 'hhhhhrrrrrrrrr', True]
+print(my_list)
+print(type(my_list))
+print(type(my_list[0]))
+print(type(my_list[1]))
+print(type(my_list[2]))
+print(type(my_list[3]))
+print(type(my_list[4]))
+print(type(my_list[5]))
+print(my_list)
+
+
